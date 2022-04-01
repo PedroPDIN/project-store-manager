5d6d5d55a3aa828864bdaf024e65bd98c530d5fa
Pedro Lima <pedro.oficial1969@gmail.com>
2022-03-31 21:49:59 -0300

    feat(middlewares): adiciona middlewares de validação.

---
+++ middlewares/isValidProducts.js
@@ -0,0 +1,31 @@
+const isValidNameProduct = (req, res, next) => {
+  const { name } = req.body;
+  const nameLength = name && name.length;
+  const LIMIT = 5;
+
+  if (!name) return res.status(400).json({ message: '"name" is required' });
+  if (nameLength >= LIMIT) {
+     return res
+      .status(422)
+      .json({ message: '"name" length must be at least 5 characters long' });
+  }
+  next();
+};
+
+const isValidQuantityProduct = (req, res, next) => {
+  const { quantity } = req.body;
+  const LIMIT = 1;
+  
+  if (!quantity) return res.status(400).json({ message: '"quantity" is required' });
+  if (quantity < LIMIT) {
+    return res
+    .status(422)
+    .json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+  next();
+};
+
+module.exports = {
+  isValidNameProduct,
+  isValidQuantityProduct,
+};
